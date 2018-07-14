bed02b27c77496e9e61a632b5aeefbe38f26b361
Douglas Roeder <douglasroeder@Douglass-MacBook-Pro.local>
2018-07-14 22:18:58 +1000

    updated lua script

--- dotnews/spiders/foxnews.py
+++ dotnews/spiders/foxnews.py
@@ -9,18 +9,40 @@ class FoxnewsSpider(scrapy.Spider):
     allowed_domains = ['foxnews.com']
     start_urls = ['http://www.foxnews.com']
 
+    script = """
+function main(splash)
+     splash.images_enabled = false
+     splash.resource_timeout = 100.0
+     splash:autoload('http://code.jquery.com/jquery-2.1.3.min.js')
+     assert(splash:go(splash.args.url))
+     splash:wait(1)
+
+     for i = 10,1,-1
+     do
+         splash:runjs("$('.js-load-more').click()")
+         splash:wait(0.5)
+     end
+
+     return splash:html()
+end
+    """
+
     def start_requests(self):
         for url in self.start_urls:
-            yield SplashRequest(url, self.parse, args={})
+            yield SplashRequest(url,
+                                self.parse,
+                                args={})
 
     def parse(self, response):
         topic_urls_xpath = '//footer/div/div/nav/ul/li/a/@href'
         urls = response.xpath(topic_urls_xpath).extract()
         for url in urls:
             if 'category' in url:
-                yield SplashRequest("https:" + url, self.parse_articles, args={})
+                yield SplashRequest('https:' + url,
+                                    self.parse_articles,
+                                    endpoint='execute',
+                                    args={'lua_source': self.script})
 
-    # TODO click 'Show more' and capture more articles
     def parse_articles(self, response):
         # meta tags
         category = response.xpath("//meta[@name='prism.subsection1']/@content")[0].extract()
@@ -32,7 +54,6 @@ class FoxnewsSpider(scrapy.Spider):
             title_xpath = './/h2[@class="title"]/a/text()'
             abstract_xpath = './/div[@class="content"]/p/a/text()'
 
-            self.log(article)
             yield {
                 'category' : category,
                 'subcategory' : subcategory,
